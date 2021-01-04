2e3619e702d97df02845abdc1854df56c69ab95a
guozhouqian <guozhouqian@beisen.com>
2021-01-04 20:21:09 +0800

    feat(链表): 修改两两交换的计算

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -49,7 +49,7 @@ export const reverseList = (l1, isConsole = false) => {
 // 链表两两交换
 /** 
 * 1、改动原链表，利用链表的updateList方法进行操作
-* 2、创建新链表，利用链表的append方法,此方法不会影响原来的链表（还需要再完善一下）
+* 2、创建新链表，利用链表的append方法,此方法不会影响原来的链表
 * @param {LinkedList} l1
 * @param {boolean} isConsole
 * @return {LinkedList}
@@ -71,24 +71,12 @@ export const changeList = (l1, isConsole = false) => {
   // 方法二：
   let cur = new LinkedList();
   for (let i = 0; i < l1.length; i++) {
-    if (l1.length % 2 === 0) {  // 偶数交换
-      if (i % 2 == 0) {
-        cur.append(l1.getElementAt(i + 1).val);
-      } else {
-        cur.append(l1.getElementAt(i - 1).val);
-      }
-    } else {  // 奇数个数交换，后面一个不交换
-      if (i !== l1.length - 1) {
-        if (i % 2 == 0) {
-          cur.append(l1.getElementAt(i + 1).val);
-        } else {
-          cur.append(l1.getElementAt(i - 1).val);
-        }
-      } else {
-        cur.append(l1.getElementAt(i).val);
-      }
+    if (i % 2 == 0 && i !== l1.length - 1) {
+      cur.append(l1.getElementAt(i + 1).val);
+      cur.append(l1.getElementAt(i).val)
     }
   }
+  cur.append(l1.getElementAt(l1.length - 1).val)
   isConsole && console.debug('=====changeList:', cur.toString());
   return cur;
 }
